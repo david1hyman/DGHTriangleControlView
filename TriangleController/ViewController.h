c6683817820c87aac57baec8d90bae442d77f84c
David Hyman <dghyman@gmail.com>
2013-08-09 10:15:30 -0500

    Initial Commit

---
+++ TriangleController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TriangleController
+//
+//  Created by D Mac on 8/9/13.
+//  Copyright (c) 2013 D Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TriangleController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TriangleController
+//
+//  Created by D Mac on 8/9/13.
+//  Copyright (c) 2013 D Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
